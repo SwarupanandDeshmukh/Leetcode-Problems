86f4995efbf6df4636a941e75d7db9ae38417211
SwarupanandDeshmukh <135790022+SwarupanandDeshmukh@users.noreply.github.com>
2026-01-13 17:25:44 +0530

    Time: 23 ms (14.16%), Space: 94.6 MB (22.99%) - LeetHub

--- 2022-convert-1d-array-into-2d-array/2022-convert-1d-array-into-2d-array.cpp
+++ 2022-convert-1d-array-into-2d-array/2022-convert-1d-array-into-2d-array.cpp
@@ -11,11 +11,13 @@ public:
             return {};
         }
         
+        int k = 0;
         for(int i = 0; i<m; i++)
         {
             for(int j = 0; j<n ;j++)
             {
-                res [i][j] = original[i*n + j];
+                res [i][j] = original[k];
+                k++;
             }
             
         }
