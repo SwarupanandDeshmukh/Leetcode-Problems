02bc0147785a1220e7af7f7a3fe0a76cede43ba1
SwarupanandDeshmukh <135790022+SwarupanandDeshmukh@users.noreply.github.com>
2026-01-19 23:07:09 +0530

    Time: 7 ms (19.51%), Space: 9.6 MB (73.79%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -4,18 +4,31 @@ public:
         if(s.length() != t.length())
             return false;
         
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
+        // sort(s.begin(), s.end());
+        // sort(t.begin(), t.end());
 
-        if(s == t)
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
+        // if(s == t)
+        // {
+        //     return true;
+        // }
+        // else
+        // {
+        //     return false;
+        // }
 
+        map<char,int> m1;
+        map<char,int> m2;
+
+        for(char ch : s)
+        {
+            m1[ch] = m1[ch] + 1;
+        }
         
+        for(char ch : t)
+        {
+            m2[ch]++;
+        }
+
+       return m1 == m2;
     }
 };
