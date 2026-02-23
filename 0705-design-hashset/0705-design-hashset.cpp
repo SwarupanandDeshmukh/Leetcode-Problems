b6456ae1d983961055e932b3a49536993d71831c
SwarupanandDeshmukh <135790022+SwarupanandDeshmukh@users.noreply.github.com>
2026-02-23 16:40:01 +0530

    Time: 71 ms (16.35%), Space: 47.6 MB (79.52%) - LeetHub

---
+++ 0705-design-hashset/0705-design-hashset.cpp
@@ -0,0 +1,59 @@
+class MyHashSet {
+public:
+    int s = 10;
+    vector<list<int>> table;
+    MyHashSet() {
+        table.resize(s);
+    }
+    
+    void add(int key) {
+        int idx = key % s;
+        auto it = table[idx].begin();
+        while(it != table[idx].end())
+        {
+            if(key == *it)
+                return;
+            it++;
+        }
+
+        table[idx].push_back(key);
+    }   
+    
+    void remove(int key) {
+         int idx = key % s;
+        auto it = table[idx].begin();
+        while(it != table[idx].end())
+        {
+            if(key == *it)
+            {
+                table[idx].remove(key);
+                return;
+            }
+            it++;
+        }
+        return;
+    
+    }
+    
+    bool contains(int key) {
+        
+        int idx = key % s;
+        auto it = table[idx].begin();
+        while(it != table[idx].end())
+        {
+            if(key == *it)
+                return true;
+            it++;
+        }
+        return false;
+
+    }
+};
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet* obj = new MyHashSet();
+ * obj->add(key);
+ * obj->remove(key);
+ * bool param_3 = obj->contains(key);
+ */
