bf751bebda2b6abc905dba9fc772fb037e0bf7a6
SwarupanandDeshmukh <135790022+SwarupanandDeshmukh@users.noreply.github.com>
2026-03-09 16:43:39 +0530

    Time: 482 ms (13.89%), Space: 0B (100%) - LeetHub

--- 1148-article-views-i/1148-article-views-i.sql
+++ 1148-article-views-i/1148-article-views-i.sql
@@ -1,3 +1,5 @@
 # Write your MySQL query statement below
 
+# author that viewed at least one of their own articles.
+
 select DISTINCT(viewer_id) as 'id' from Views where viewer_id = author_id ORDER BY author_id ASC;
