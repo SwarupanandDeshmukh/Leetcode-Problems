e613a0217ee112deb0075b563107ce4f425f68e4
SwarupanandDeshmukh <135790022+SwarupanandDeshmukh@users.noreply.github.com>
2026-04-13 17:53:39 +0530

    Time: 0 ms (100%), Space: 31.4 MB (17.97%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
@@ -12,28 +12,65 @@
 class Solution {
 public:
     int countNodes(TreeNode* root) {
+        // if(root == NULL)
+        //     return 0;
+        
+        // queue<TreeNode* > q;
+        // int cnt = 0;
+        // q.push(root);
+
+        // while(!q.empty())
+        // {
+        //     TreeNode* temp = q.front();
+        //     q.pop();
+
+        //     cnt++;
+
+        //     if(temp->left)
+        //         q.push(temp->left);
+            
+        //     if(temp->right)
+        //         q.push(temp->right);
+
+        // }
+
+        // return cnt;
+
         if(root == NULL)
             return 0;
-        
-        queue<TreeNode* > q;
-        int cnt = 0;
-        q.push(root);
+        int leftcnt = leftCount(root);
+        int rightcnt = rightCount(root);
 
-        while(!q.empty())
+        if(leftcnt == rightcnt)
+            return (int) pow(2, leftcnt) - 1;
+        else
+            return 1 + countNodes(root->left) + countNodes(root->right);
+
+     }
+
+     int leftCount(TreeNode *root)
+     {
+        int d = 0;
+        while(root != NULL)
         {
-            TreeNode* temp = q.front();
-            q.pop();
-
-            cnt++;
-
-            if(temp->left)
-                q.push(temp->left);
-            
-            if(temp->right)
-                q.push(temp->right);
-
+            root = root->left;
+            d++;
         }
 
-        return cnt;
-    }
+        return d;
+    
+     }
+
+     int rightCount(TreeNode *root)
+     {
+        int d = 0;
+        while(root != NULL)
+        {
+            root = root->right;
+            d++;
+        }
+
+        return d;
+    
+     }
 };
