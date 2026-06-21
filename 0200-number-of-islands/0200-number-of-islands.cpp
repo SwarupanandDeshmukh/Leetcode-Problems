6dbe836917bd72be77e6cccba25242c31bd0c5e1
SwarupanandDeshmukh <135790022+SwarupanandDeshmukh@users.noreply.github.com>
2026-06-21 11:14:39 +0530

    Time: 20 ms (94.21%), Space: 16.6 MB (63.12%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -8,28 +8,28 @@ public:
         return false;
     }
 
-    void dfshelper(int sr, int sc,map<pair<int, int>, bool> &visited,vector<vector<char>> &grid, int m, int n)
+    void dfshelper(int sr, int sc,vector<vector<bool>> & visited,vector<vector<char>> &grid, int m, int n)
     {
-        visited[{sr,sc}] = true;
+        visited[sr][sc] = true;
 
         //calling the function recursively for neighbours.
 
-        if(valid(sr+1,sc,m,n) && grid[sr+1][sc] == '1' && !visited[{sr+1,sc}])
+        if(valid(sr+1,sc,m,n) && grid[sr+1][sc] == '1' && !visited[sr+1][sc])
         {
             dfshelper(sr+1,sc, visited, grid, m ,n);
         }
 
-        if(valid(sr-1, sc, m,n) && grid[sr-1][sc] == '1' && !visited[{sr-1,sc}])
+        if(valid(sr-1, sc, m,n) && grid[sr-1][sc] == '1' && !visited[sr-1][sc])
         {
             dfshelper(sr-1,sc, visited, grid, m ,n);
         }
 
-        if(valid(sr,sc+1,m,n) && grid[sr][sc+1] == '1' && !visited[{sr,sc+1}])
+        if(valid(sr,sc+1,m,n) && grid[sr][sc+1] == '1' && !visited[sr][sc+1])
         {
             dfshelper(sr,sc+1, visited, grid, m ,n);
         }
 
-        if(valid(sr,sc-1,m,n) && grid[sr][sc-1] == '1' && !visited[{sr,sc-1}])
+        if(valid(sr,sc-1,m,n) && grid[sr][sc-1] == '1' && !visited[sr][sc-1])
         {
             dfshelper(sr,sc-1, visited, grid, m ,n);
         }
@@ -41,14 +41,14 @@ public:
         int m = grid.size();
         int n = grid[0].size();
 
-        map<pair<int, int>, bool> visited;
+        vector<vector<bool>> visited(m, vector<bool> (n,false));
         int cnt = 0;
 
         for(int i = 0; i<m; i++)
         {
             for(int j = 0; j<n; j++)
             {
-                if(grid[i][j] == '1' && !visited[{i,j}])
+                if(grid[i][j] == '1' && !visited[i][j])
                 {
                     dfshelper(i,j,visited,grid, m, n);
                     cnt++;
