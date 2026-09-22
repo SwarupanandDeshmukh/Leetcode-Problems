29b6a2fbec5542b62d07402d12c3939f640ff8e8
SwarupanandDeshmukh <135790022+SwarupanandDeshmukh@users.noreply.github.com>
2026-09-22 21:30:08 +0530

    Time: 1039 ms (40.53%), Space: 0B (100%) - LeetHub

--- 1204-last-person-to-fit-in-the-bus/1204-last-person-to-fit-in-the-bus.sql
+++ 1204-last-person-to-fit-in-the-bus/1204-last-person-to-fit-in-the-bus.sql
@@ -1,8 +1,9 @@
-select person_name from
+# Write your MySQL query statement below
+with cte as
 (
-    select person_name,
-            weight,
-            SUM(weight) OVER (order by turn) as total
-    from Queue
-) as t
-where total <= 1000 order by total desc limit 1;
+    select person_name, weight, SUM(weight) over(order by turn) as cumulative from 
+    Queue order by turn
+)
+
+select person_name from cte where cumulative <= 1000 order by cumulative desc limit 1;
+
